0f67566fb1669498e6dd336abdb8a15f8b5f6969
Krishna Mohan <mohankri@gmail.com>
2018-06-12 02:25:14 -0700

    update description fix

--- excel-parser/parser.py
+++ excel-parser/parser.py
@@ -17,9 +17,10 @@ def pg150ultrascalememoryippdf():
 		if (cell_obj.value == "NAME"):
 			flag = 1
 		elif (flag) and (cell_obj.value != xlrd.empty_cell.value) :
-			output.write("#Description: " + worksheet.cell(row, 2).value + "\n")		
-			output.write("#define " + cell_obj.value + "\t" + worksheet.cell(row, 1).value + "\n")		
 			print('(%s) value %s' %(row, cell_obj.value)) 
+			if (worksheet.cell(row, 2).value != xlrd.empty_cell.value):
+				output.write("#Description: " + unicode(worksheet.cell(row, 2).value).encode('utf-8') + "\n")
+			output.write("#define " + cell_obj.value + "\t" + worksheet.cell(row, 1).value + "\n")		
 
 	output.close()
 	return
--- excel-parser/pg150.h
+++ excel-parser/pg150.h
@@ -1,23 +1,36 @@
 #ECC Register Memory Map
 #Description: ECC Status Register
 #define ECC_STATUS	0x00
-#Description: 
+#Description: ECC Enable Interrupt Register
 #define ECC_EN_IRQ	0x04
-#Description: 
 #define ECC_ON_OFF	0x08
-#Description: 
+#Description: Correctable Error Count Register
 #define CE_CNT	0x0C
-#Description: 
 #define RESERVED_01	0x10
-#Description: 
-#define CE_FFD_31_	0x100
-#Description: 
+#Description: Correctable Error First Failing Data Register
+#define CE_FFD_31	0x100
+#Description: Correctable Error First Failing Data Register
 #define CE_FFD_63	0x104
-#Description: 
+#Description: Correctable Error First Failing Data Register
 #define CE_FFD_95	0x108
-#Description: 
+#Description: Correctable Error First Failing Data Register
 #define CE_FFD_127	0x10C
-#Description: 
 #define RESERVED_02	0x110
-#Description: 
+#Description: Correctable Error First Failing ECC Register
 #define CE_FFE	0x180
+#define UE_FFD_31	0x200
+#define UE_FFD_63	0x204
+#define UE_FFD_95	0x208
+#define UE_FFD_127	0x20C
+#define RESERVED_05	0x210
+#define UE_FFE	0x280
+#define RESERVED_06	0x284
+#define UE_FFA_31	0x2C0
+#define UE_FFA_63	0x2C4
+#define RESERVED_07	0x2C8
+#define FI_D_31	0x300
+#define FI_D_63	0x304
+#define FI_D_95	0x308
+#define FI_D_127	0x30C
+#define RESERVED_08	0x340
+#define FI_ECC	0x380
